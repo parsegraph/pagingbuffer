a2f263eb1a454fe33fc5426908f6f19f884a2b55
Aaron Faanes <dafrito@gmail.com>
2022-02-21 15:12:06 -0600

    upgrade-microproject

--- publish-prod.sh
+++ publish-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PUBLISH="npm pub parsegraph-pagingbuffer-dev.tgz --tag=dev"
+PUBLISH="npm pub parsegraph-pagingbuffer-prod.tgz"
 $PUBLISH && exit
 for try in `seq 1 3`; do
     sleep $try
